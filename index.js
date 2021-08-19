04cceb58f2831e8563f0ceccd38904bf2afeadce
BimoSora <bimosora3@gmail.com>
2021-08-19 08:34:33 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1995,3 +1995,7 @@ bot.on('inline_query',async(ctx)=>{
 })
  
 bot.launch()
+
+// Enable graceful stop
+process.once('SIGINT', () => bot.stop('SIGINT'))
+process.once('SIGTERM', () => bot.stop('SIGTERM'))
