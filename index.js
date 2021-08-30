d63d05b13215b4dcee0b31d1ed14fe43ceb91428
BimoSora <bimosora3@gmail.com>
2021-08-30 15:56:49 +0700

    Update index.js

--- index.js
+++ index.js
@@ -867,7 +867,7 @@ bot.command('rem', (ctx) => {
         //console.log(text);
         if(ctx.from.id == config.ADMIN || ctx.from.id == config.ADMIN1 || ctx.from.id == config.ADMIN2){
             saver.removeFile(text)
-            ctx.reply('✅ 1 media dihapus berhasil')
+            ctx.reply('❌ 1 media dihapus berhasil')
         }
     }
 })
@@ -883,7 +883,7 @@ bot.command('remgrp', (ctx) => {
         //console.log(text);
         if(ctx.from.id == config.ADMIN || ctx.from.id == config.ADMIN1 || ctx.from.id == config.ADMIN2){
             saver.removeFileMedia(text)
-            ctx.reply('✅ Grup dihapus berhasil')
+            ctx.reply('❌ Grup dihapus berhasil')
         }
     }
 })
@@ -894,7 +894,7 @@ bot.command('clear',(ctx)=>{
     if(ctx.chat.type == 'private') {
         if(ctx.from.id == config.ADMIN || ctx.from.id == config.ADMIN1 || ctx.from.id == config.ADMIN2){
             saver.deleteCollection()
-            ctx.reply('✅ Semua media dihapus berhasil')
+            ctx.reply('❌ Semua media dihapus berhasil')
         }
     }
 })
@@ -911,7 +911,7 @@ bot.command('remall', (ctx) => {
         let id = parseInt(text)
         if(ctx.from.id == config.ADMIN || ctx.from.id == config.ADMIN1 || ctx.from.id == config.ADMIN2){
             saver.removeUserFile(id)
-            ctx.reply('✅ Hapus semua media pada pengguna berhasil')
+            ctx.reply('❌ Hapus semua media pada pengguna berhasil')
         }
     }
 })
