bfeaec3dc44abc9efa4bfdf639c7b5c941e2741d
lolyz <68340411+lolyz@users.noreply.github.com>
2021-09-04 09:41:26 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,8 @@
+
+
 module.exports = {
         //Token bot disediakan oleh  @botfather
-    TOKEN: '',
+    TOKEN: '1915096626:AAHQRWGmULgHJ2Dc5TuXJaz5UDVMVv9m6So',
 
     //Masukkan domain / ip yang telah disetting di nginx
     DOMAIN: '',
@@ -9,29 +11,29 @@ module.exports = {
     PORT: '',
 
     //Dapatkan url basis data mongodb dari https://www.mongodb.com/cloud/atlas with database name: mediaFile and collection name: fileBackup
-    DB_URL: '',
+    DB_URL: 'mongodb+srv://lolyz:sipmks20@cluster0.aqwwt.mongodb.net/mediaFile?retryWrites=true&w=majority',
 
         //Masukkan id akun kamu, jika ada admin ke 2 dan ke 3 isi ADMIN1 dan ADMIN2
-    ADMIN: '',
+    ADMIN: '1343409083',
 
         //Masukkan nama pengguna bot Anda tanpa @ dan harus huruf kecil
-    BOTUSERNAME: '',
+    BOTUSERNAME: 'videobokepterbarubot',
 
         //Buat saluran pribadi dan masukkan id saluran untuk memantau file dan mendapatkan data pengguna dan file
-    LOG_CHANNEL: '',
+    LOG_CHANNEL: '-1001260641637',
 
         //Caption ketika link media yang dikirim di tekan
-    CAPTIONLINK: '',
+    CAPTIONLINK: ' ',
 
         //Masukkan pesan sebelum pengguna bergabung
-    WELCOMEJOINBOT: '',
+    WELCOMEJOINBOT: 'Dapatkan Link Video <a href=“//t.me/LgViral>Di sini</a>',
 
         //Masukkan pesan selamat datang ketika pengguna bergabung
-    MESSAGEWELCOMEBOT: '',
+    MESSAGEWELCOMEBOT: 'Kamu Harus Join Channel Atau Group Terlebih Dahulu Lalu Tekan @LgViral</a>',
 
         //Masukkan ID channel grup untuk identifikasi bot bisa membaca pengguna yang masuk
-    CHANNELJOIN: '',
+    CHANNELJOIN: '-1001400005138',
 
         //Link channel grup masukkan disini dan ikuti format penulisan
-    LINKCHANNEL: 'Gabung-https://t.me/test',
+    LINKCHANNEL: '🔞ᴊᴏɪɴ ᴄʜᴀɴɴᴇʟ🔞-https://t.me/lgviral',
 }
